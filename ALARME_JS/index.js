bb115da127d69a5400c81a0daa745d7c2509c93a
Lincoln Baroni Trivelato <142551833+lincolnbaroni@users.noreply.github.com>
2024-10-24 22:23:51 -0300

    Estudos com JavaScript

--- ALARME_JS/index.js
+++ ALARME_JS/index.js
@@ -1,46 +1,43 @@
-    let display = document.getElementById('display')
     let minutos = document.getElementById('minutos')
     let segundos = document.getElementById('segundos')
     let comecar = document.getElementById('comecar')
+    let display = document.getElementById('display')
     let sound = document.getElementById('sound')
 
     let minutoAtual
     let segundoAtual
-
     let interval
 
     for(let i = 0; i <= 60; i++) {
-        minutos.innerHTML += '<option value="'+i+'">'+i+'</option>';
+        minutos.innerHTML += '<option value="'+i+'">'+i+'</option>'
     }
 
-    for (let i = 1; i <=60; i++) {
-        segundos.innerHTML += '<option value="'+i+'">'+i+'</option>';
+    for(let i = 1; i < 60; i++) {
+        segundos.innerHTML += '<option value="'+i+'">'+i+'</option>'
     }
 
-    comecar.addEventListener('click', function() {
+    comecar.addEventListener('click',function() {
         minutoAtual = parseInt(minutos.value)
         segundoAtual = parseInt(segundos.value)
+    
 
+    display.childNodes[1].innerHTML = minutoAtual + ':' + segundoAtual
 
-    display.childNodes[1].innerHTML = minutoAtual + ":" + segundoAtual
-
-    interval = setInterval(function(){
+    interval = setInterval(function() {
 
         segundoAtual--
-        if(segundoAtual < 0) {
+        if(segundoAtual <= 0) {
             if(minutoAtual > 0) {
                 minutoAtual--
                 segundoAtual = 59
             } else {
-                alert('acabou')
-                sound = document.getElementById('sound').play()
+                alert('Acabou')
+                document.getElementById('sound').play()
                 clearInterval(interval)
             }
-
         }
-
-        display.childNodes[1].innerHTML = minutoAtual + ":" + segundoAtual
-    },1000)
+        display.childNodes[1].innerHTML = minutoAtual + ':' + segundoAtual
+    }, 1000)
 
 
 })
