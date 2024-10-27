94f2cb5b6b4261e95d203f613342f7a52131871a
Lincoln Baroni Trivelato <142551833+lincolnbaroni@users.noreply.github.com>
2024-10-26 23:07:59 -0300

    Iniciado estudos com JQuery

--- mini_loja/index.js
+++ mini_loja/index.js
@@ -29,10 +29,38 @@ inicializarLoja = () => {
         <div class="produto-single">
             <img src="`+val.img+`" />
             <p>`+val.nome+`</p>
-            <a key="`+val.id+`" href="">Adicionar ao carrinho</a>
+            <a key="`+val.id+`" href="#">Adicionar ao carrinho</a>
         </div>
        `
     })
 }
 
 inicializarLoja()
+
+atualizarCarrinho = () => {
+    let containerCarrinho = document.getElementById('carrinho')
+    containerCarrinho.innerHTML = ""
+    items.map((val)=> {
+        if(val.quantidade > 0) {
+       containerCarrinho.innerHTML += `
+       
+        <p>`+val.nome+` | quantidade: `+val.quantidade+`</p>
+        <hr>
+       `;
+        }
+    })
+}
+
+
+
+
+let links = document.getElementsByTagName('a')
+
+for(let i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', function() {
+        let key = this.getAttribute('key')
+        items[key].quantidade++
+        atualizarCarrinho()
+        return false
+    })
+}
