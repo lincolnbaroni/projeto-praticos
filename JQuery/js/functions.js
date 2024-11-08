71f62c2a6430638d929685427a8543d99f4d5914
Lincoln Baroni Trivelato <142551833+lincolnbaroni@users.noreply.github.com>
2024-11-07 22:34:04 -0300

    Atualizado

--- JQuery/js/functions.js
+++ JQuery/js/functions.js
@@ -1,3 +1,25 @@
-$(window).on('load', function() {
-    alert('ola mundo')
+$(function() {
+
+    function validarClickEHover() {
+        $('.artigo1').click(function() {
+        $('.artigo2').css('background-color','red')
+        })
+
+        $('.artigo1').hover(function() {
+        $('.artigo2').css('background-color','blue')
+        }, function() {
+        $('.artigo2').css('background-color','black')
+        })
+    }
+
+    
+    function validarFormulario() {
+        $('select').change(function() {
+            console.log('Meu select foi alterado')
+        })
+    }
+
+    validarFormulario()
+    validarClickEHover()
+
 })
