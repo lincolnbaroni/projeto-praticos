7888a22c2db7deea3842cbb1af14e82fc74d9301
Lincoln Baroni Trivelato <142551833+lincolnbaroni@users.noreply.github.com>
2024-11-11 22:45:53 -0300

    Estudos com JQuery

--- JQuery/js/functions.js
+++ JQuery/js/functions.js
@@ -1,25 +1,7 @@
 $(function() {
-
-    function validarClickEHover() {
-        $('.artigo1').click(function() {
+    $('.artigo1').hover(function() {
         $('.artigo2').css('background-color','red')
-        })
-
-        $('.artigo1').hover(function() {
+    }, function() {
         $('.artigo2').css('background-color','blue')
-        }, function() {
-        $('.artigo2').css('background-color','black')
-        })
-    }
-
-    
-    function validarFormulario() {
-        $('select').change(function() {
-            console.log('Meu select foi alterado')
-        })
-    }
-
-    validarFormulario()
-    validarClickEHover()
-
+    })
 })
