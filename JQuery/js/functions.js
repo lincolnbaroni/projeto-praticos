f37c7b42e1c56fb8c0065bbda618152e28102300
Lincoln Baroni Trivelato <142551833+lincolnbaroni@users.noreply.github.com>
2024-11-17 00:25:58 -0300

    Estudos com php

--- JQuery/js/functions.js
+++ JQuery/js/functions.js
@@ -1,26 +1,5 @@
 $(function() {
-
-
-    /*
-        let time
-
-    $(window).resize(function() {
-        clearTimeout(time)
-        time = setTimeout(function() {
-            location.href ="file:///C:/Users/Lincoln%20Baroni/OneDrive/Desktop/ESTUDOS/estudy-front%20end/projeto-praticos/JQuery/index.html"
-        }, 2000)
-    })
-
-    $('a').click(function(e) {
-        e.preventDefault()
-    })
-   */
-
-    $('.box').click(function(e) {
-        e.stopPropagation()
-    })
-
-    $('body').click(function() {
-        $('.box').css('background-color','green')
+    $(window).scroll(function() {
+        console.log('scroll ativo')
     })
 })
