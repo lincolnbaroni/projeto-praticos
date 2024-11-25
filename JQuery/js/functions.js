d2afc09ed3daf22046821c6fa8ce85453343a4ad
Lincoln Baroni Trivelato <142551833+lincolnbaroni@users.noreply.github.com>
2024-11-24 22:06:26 -0300

    Estudos com JQuery

--- JQuery/js/functions.js
+++ JQuery/js/functions.js
@@ -1,3 +1,12 @@
 $(window).on('load',function() {
-    let elWidth = $('.box').width()
+   
+   //let elWidth = $('.box').width()
+   $('.box').width('900')
+   $('.box').css('width','900px')
+   console.log("Width: "+ $('.box').width())
+  
+    console.log("InnerWidth: " + $('.box').innerWidth())
+    console.log("OuterWidth: " + $('.box').outerWidth(true))
+
+
 })
