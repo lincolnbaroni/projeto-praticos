9d2cdb811d99d0fd287ff99a2d2ec821604b5e90
Lincoln Baroni Trivelato <142551833+lincolnbaroni@users.noreply.github.com>
2024-11-28 21:32:55 -0300

    Mais estudos com JQuery

--- JQuery/js/functions.js
+++ JQuery/js/functions.js
@@ -1,34 +1,12 @@
-$(window).on('load',function() {
-   
-  /*
+$(document).ready(function() {
+
   let el = $('.box')
 
-  let meutexto = 'Ola mundo'
-  el.html("<div class=\"teste\">"+meutexto+"</div>")
-  el.html(el.html() + '<h1 class="texto1">Meu primeiro texto dinamico em JQuery</h1>')
-  
-    $('.box2').text("<div></dvi>")
-    $('.box2').text($('.box2').text() + 'Ola mundo')
-    console.log($('.box2').text())
+  let meutexto = 'ola mundo'
+  el.html("<h1 class=\"texto1\">"+meutexto+"</h1>")
 
-    $('input[type=text]').val('ola mundo')
-    $('textarea').text('ola mundo')
-    */
-
-
-    $('input[type=button]').click(function() {
-        let str = $('input[type=text]').val()
-        //let var2 = v.split('@')
-        //console.log(var2)
-        //console.log(str.substr(1,4))
-
-        let splitstr = str.split('@')
-        if(splitstr[1] == 'hotmail.com') {
-          $('input[type=text]').css('opacity','0')
-        } else {
-          console.log('A condição nao bateu')
-        }
-        
-    })
+  let texto2 = 'Segundo texto'
+  el.html(el.html() + '<div class="texto2">'+texto2+'</div>')
+  el.html(texto2).css('font-size','20px')
 
 })
