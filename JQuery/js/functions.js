da6fbaff388bca0251a361b5ec6bc626e8558094
Lincoln Baroni Trivelato <142551833+lincolnbaroni@users.noreply.github.com>
2024-11-30 11:09:02 -0300

    Mais estudos e aplicação com JQuery

--- JQuery/js/functions.js
+++ JQuery/js/functions.js
@@ -1,12 +1,37 @@
 $(document).ready(function() {
 
+  /*
   let el = $('.box')
 
   let meutexto = 'ola mundo'
   el.html("<h1 class=\"texto1\">"+meutexto+"</h1>")
+   
 
   let texto2 = 'Segundo texto'
   el.html(el.html() + '<div class="texto2">'+texto2+'</div>')
-  el.html(texto2).css('font-size','20px')
+
+  $('.box2').text('Ola mundo')
+  $('.box2').text($('.box2').text() +  'Tudo bem com voce')
+  console.log($('.box2').text())
+
+  $('input[type=text]').text('ola mundo')
+  $('textarea').text('ola tudo bem')
+  */
+
+  $('input[type=button]').click(function() {
+    let v = $('input[type=text]').val()
+    //let v2 = v.split('@')
+   //console.log(v.substr(3,2))
+
+   let splitstr = v.split('@')
+
+   if(splitstr[1] == 'hotmail.com') {
+      $('input[type=text]').css('opacity','0')
+   } else {
+    console.log('A condição não bateu')
+   }
+     
+  }) 
+  
 
 })
