01c8552744d5db368444fb742956fdb87bea28c5
Lincoln Baroni Trivelato <142551833+lincolnbaroni@users.noreply.github.com>
2024-12-02 21:55:02 -0300

    Estudos com JQuery

--- JQuery/js/functions.js
+++ JQuery/js/functions.js
@@ -1,37 +1,32 @@
 $(document).ready(function() {
 
   /*
-  let el = $('.box')
+  let v = $('.box')
+  let texto = 'ola mundo'
+  v.html("<div class=\"test1\">"+texto+"</div>")
 
-  let meutexto = 'ola mundo'
-  el.html("<h1 class=\"texto1\">"+meutexto+"</h1>")
-   
+  v.html(v.html()+ '<h1 class="texto1">Meu texto via JavaScript</div>')
 
-  let texto2 = 'Segundo texto'
-  el.html(el.html() + '<div class="texto2">'+texto2+'</div>')
+  $('.box2').text('<div></div>')
+  $('.box2').text($('.box2').text() + 'ola mundo')
 
-  $('.box2').text('Ola mundo')
-  $('.box2').text($('.box2').text() +  'Tudo bem com voce')
   console.log($('.box2').text())
 
-  $('input[type=text]').text('ola mundo')
-  $('textarea').text('ola tudo bem')
+  $('input[type=text]').val('ola mundo')
+  $('textarea').text('ola mundo')
   */
 
   $('input[type=button]').click(function() {
     let v = $('input[type=text]').val()
     //let v2 = v.split('@')
-   //console.log(v.substr(3,2))
+    //console.log(v2)
 
-   let splitstr = v.split('@')
-
-   if(splitstr[1] == 'hotmail.com') {
+    let splitstr = v.split('@')
+    if(splitstr[1] == 'hotmail.com') {
       $('input[type=text]').css('opacity','0')
-   } else {
-    console.log('A condição não bateu')
-   }
-     
-  }) 
-  
+    } else {
+      console.log('condição não foi valida')
+    }
+  })
 
-})
+}) 
