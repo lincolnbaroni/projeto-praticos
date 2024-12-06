f8801f6616508531c45ab3f2a7efdc0748745f92
Lincoln Baroni Trivelato <142551833+lincolnbaroni@users.noreply.github.com>
2024-12-05 22:23:48 -0300

    Estudos com JQuery

--- JQuery/js/functions.js
+++ JQuery/js/functions.js
@@ -1,19 +1,6 @@
 $(document).ready(function() {
 
-  //$('.box div').append('<h3>Meu elemento adicionado</h3>')
+ $('a').click(function() {
 
-  //let el = $('<h3>Meu conteudo</h3>').appendTo($('.box'))
-  //el.css('color','red')
-
-  //$('.box').prepend('<h3> meu conteudo 1 </h3>')
-  //let el = $('<h3> meu conteudo </h3>').prependTo($('.box'))
-  //el.css('font-size','30px')
-
-let t = ('<p>Meu conteudo depois da variavel</p>')
- // $('.box').after(t)
-
- // $('.box').before(t)
-
-$(t).insertAfter($('.box')).css('color','red')
-$(t).insertBefore($('.box')).css('color','red')
+ })
 }) 
