69f1cc131b4075bdd1e0bcf68b02b31315087e3a
Lincoln Baroni Trivelato <142551833+lincolnbaroni@users.noreply.github.com>
2024-12-07 19:14:34 -0300

    Estudos com JQuery, mais eventos

--- JQuery/js/functions.js
+++ JQuery/js/functions.js
@@ -1,17 +1,13 @@
 $(document).ready(function() {
 
-    
-    $('.form_contato').submit(function() {
-        let container = $('.container')
-        let content = 'Nome: '+ $('input[name=nome]').val() +
-        '<hr>E-mail: ' + $('input[name=email]').val()+
-        '<hr>Telefone: ' +$('input[name=telefone]').val()
+   $('.form').submit(function() {
+    let container = $('.container')
+    let content = 'Nome: '+ $('input[type=text]').val() +
+    '<hr>E-mail: '+ $('input[type=email]').val() +
+    '<hr>Telefone: '+ $('input[type=tel]').val()
 
+    container.html(content)
 
-        container.html(content)
-        return false
-    })
-
-    
-
+    return false
+   })
 }) 
