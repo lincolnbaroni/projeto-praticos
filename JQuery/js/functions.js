733a0ba5f00c41fd8c55e3a89e5fd53588c36435
Lincoln Baroni Trivelato <142551833+lincolnbaroni@users.noreply.github.com>
2024-12-06 23:20:49 -0300

    Estudos de eventos e funções JQuery

--- JQuery/js/functions.js
+++ JQuery/js/functions.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
 
- $('a').click(function() {
+    
+    $('.form_contato').submit(function() {
+        let container = $('.container')
+        let content = 'Nome: '+ $('input[name=nome]').val() +
+        '<hr>E-mail: ' + $('input[name=email]').val()+
+        '<hr>Telefone: ' +$('input[name=telefone]').val()
 
- })
+
+        container.html(content)
+        return false
+    })
+
+    
+
 }) 
