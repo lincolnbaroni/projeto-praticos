4c441b724727e58dace11a6842a120d9eed668ce
Lincoln Baroni Trivelato <142551833+lincolnbaroni@users.noreply.github.com>
2024-12-12 21:43:09 -0300

    Funções de animaçõs e classe JQuery

--- JQuery/js/functions.js
+++ JQuery/js/functions.js
@@ -1,34 +1,8 @@
 $(document).ready(function() {
 
-   /*
-   let timer
-   let timeOut = function() {
-      $('.box2').animate({
-      'width':'40%',
-      'height':'500px',
-      'padding':'35px',
-      'marginLeft':'490px'
-      },2000)
-   }
- 
-   $('body').click(function() {
-      alert('animaçãop foi cancelada')
-      clearTimeout(timer)
-   })
+   $('.box1').addClass('minhaClasse')
 
-   $('.box1').animate({
-      'width':'40%',
-      'height':'500px',
-      'padding':'35px',
-      'marginLeft':'490px'
-   }, 2000, function() {
-      timer = setTimeout(timeOut, 3000)
-   })
-      */
-
-   let timer
-   timer = setInterval(function() {
-      alert('ola bem vindo')
-   },3000)
+   // $('.minhaClasse').remove()
+   $('.box1').removeClasse('minhaClasse')
   
 }) 
