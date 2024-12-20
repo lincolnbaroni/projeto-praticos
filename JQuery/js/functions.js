50266f34475f5ac1fa8854625b1c6c4c4908a68b
Lincoln Baroni Trivelato <142551833+lincolnbaroni@users.noreply.github.com>
2024-12-19 21:02:29 -0300

    Manipulando class com JQuery

--- JQuery/js/functions.js
+++ JQuery/js/functions.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
 
    $('.box1').addClass('minhaClasse')
 
-   
    $('.box1').removeClass('minhaClasse')
+   // $('.box1').removeClass('minhaClasse')
 
-   let el = $('.box1').find('.child1').find('.child2')
-   el.css('color','green')
+   // let el = $('.box1').find('.child1').find('.child2')
+   // el.css('color','green')
 
    $('minhatag').attr('meuattr','outrovalor')
    alert($('minhatag').attr('meuattr'))
